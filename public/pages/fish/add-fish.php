bcc9be3191d5530aaf3d405574c0f4d7feb52649
Jomer Dorego <jdorego06@gmail.com>
2023-05-31 21:19:48 +0800

    add ckeditor library on add fish description

--- public/pages/fish/add-fish.php
+++ public/pages/fish/add-fish.php
@@ -41,8 +41,8 @@
 
             <div class="flex justify-between py-3 px-10">
                <div class="username w-[100%]">
-                  <label for="username" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Fish Information</label>
-                  <textarea type="text" name="username" id="username" placeholder="Enter a description or some information about the fish..." class="bg-gray-50 block border min-h-[20rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3"></textarea>
+                  <label for="eventDesc" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Fish Information</label>
+                  <textarea type="text" name="username" id="eventDesc" placeholder="Enter a description or some information about the fish..." class="bg-gray-50 block border min-h-[20rem] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3"></textarea>
                </div>
               
             </div>     
--- public/pages/partials/header.php
+++ public/pages/partials/header.php
@@ -7,6 +7,7 @@
    <link rel="icon" type="image/x-icon" href="<?= ROOT_URL ?>/public/assets/images/logo-fishdroid.webp">
    <link rel="stylesheet" href="<?= ROOT_URL ?>/public/assets/style/output.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <script src="https://cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
    <title>Index</title>
 </head>
 <body class="flex overflow-hidden">
