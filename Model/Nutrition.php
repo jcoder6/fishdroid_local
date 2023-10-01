8f00ee4a757793f5b8a1ed8f90e82b93fc558f89
Mark Lester Taguiam <lestertaguiam1107@gmail.com>
2023-10-01 18:58:04 +0800

    add the CRUD feature in the nutrition

---
+++ Model/Nutrition.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Makkari\Models;
+
+use Makkari\Models\Model;
+require_once './Model/Model.php';
+class Nutrition extends Model
+{
+    protected $id;
+    protected $fish_id;
+    protected $nutrition_name;
+
+    public function __construct($id,$fish_id,$nutrition_name)
+    {
+        parent::__construct();
+           $this->id=$id;
+    $this->fish_id=$fish_id;
+    $this->nutrition_name=$nutrition_name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFish_id()
+    {
+        return $this->fish_id;
+    }
+
+    public function getNutrition_name()
+    {
+        return $this->nutrition_name;
+    }
+
+    public function setId($value)
+    {
+        $this->id = $value;
+    }
+
+    public function setFish_id($value)
+    {
+        $this->fish_id = $value;
+    }
+
+    public function setNutrition_name($value)
+    {
+        $this->nutrition_name = $value;
+    }
+
+    public static function getAll(){
+        $m = new Model;
+        $list = [];
+        $r = $m->all('nutritions');
+        if($r){
+            foreach($r as $v){
+                $data = new Nutrition(
+                    $v->id,
+                    $v->fish_id,
+                    $v->nutrition_name
+                );
+                $list[] = $data;
+            }
+        }
+        return $list;
+    }
+
+    public static function getById($value){
+        $m = new Model;
+        $data = NULL;
+        $r = $m->getOne('nutritions','id', $value);
+        if($r){
+            
+            $data = new Nutrition(
+                $r->id,
+                $r->fish_id,
+                $r->nutrition_name
+            );
+            
+        }
+        return $data;
+    }
+
+    public static function getAllById($value){
+        $m = new Model;
+        $list = [];
+        $r = $m->allById('nutritions','fish_id', $value);
+        if($r){
+            foreach($r as $v){
+                $data = new Nutrition(
+                    $v->id,
+                    $v->fish_id,
+                    $v->nutrition_name
+                );
+                $list[] = $data;
+            }
+        }
+
+        return $list;
+    }
+
+    public function save(){
+        if($this->id){
+            $query = 'UPDATE nutritions SET fish_id=:fish_id,nutrition_name=:nutrition_name WHERE id=:id';
+            $params = array(':id'=>$this->id,':fish_id'=>$this->fish_id,':nutrition_name'=>$this->nutrition_name);
+            $result = $this->executeQuery($query,$params);
+            return $result;
+        }else{
+            $query = 'INSERT INTO nutritions VALUES (:id,:fish_id,:nutrition_name)';
+            $params = array(':id'=>$this->id,':fish_id'=>$this->fish_id,':nutrition_name'=>$this->nutrition_name);
+            $result = $this->executeQuery($query,$params);
+            return $result;
+        }
+    }
+
+    public function remove()
+    {
+        if($this->id){
+            $stmt=$this->delete('nutritions',$this->id);
+            return $stmt->rowCount();
+        }
+    }
+}
+
--- public/pages/nutrition/delete-fish-nutrition.php
+++ public/pages/nutrition/delete-fish-nutrition.php
@@ -4,12 +4,12 @@
 <div class="admin-feature-container w-full h-screen overflow-auto px-3">
     <?php include PAGES_PATH . '/partials/body-header.php' ?>
         <div class="contents-container w-[70%] mt-[-60px] bg-slate-200 mx-auto rounded-lg"> 
-            <h4 class="text-2xl text-center font-bold text-gray-700 p-3">Bangus Nutritions</h4>
+            <h4 class="text-2xl text-center font-bold text-gray-700 p-3"><?= $fish->getFish_name() ?> Nutritions</h4>
             
-            <form action="/familyNames/save" method="post">
+            <form action="/nutritions/save/<?= $fishID ?>" method="post">
                 <div class="add-family-name bg-gradient-to-bl from-green-400 via-green-500 to-green-600 b p-2 flex justify-center gap-2 items-center text-center mt-5">
-                    <label for="family_name" class="text-left text-sm font-bold text-slate-100">Add Family Name:</label>
-                    <input type="text" name="family_name" placeholder="Enter Family Name" id="family_name" class="bg-slate-100 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-3 w-[40%]" required>
+                    <label for="nutrition_name" class="text-left text-sm font-bold text-slate-100">Add Family Name:</label>
+                    <input type="text" name="nutrition_name" placeholder="Enter Family Name" id="nutrition_name" class="bg-slate-100 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 p-1 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ml-3 w-[40%]" required>
                     <input type="submit" value="Add" class="transition-all font-semibold text-gray-700 py-1 px-4 text-sm rounded-md cursor-pointer bg-white hover:bg-gradient-to-tr from-teal-400 via-teal-500 to-teal-600 hover:text-white">
                 </div>
             </form>
@@ -19,17 +19,17 @@
                     <h1 class="text-center text-xl font-semibold text-gray-700 p-2">Fish Information</h1>
                     <div class="bg-white p-3 rounded-lg shadow-md mb-10 min-h-[26rem]">
                         <div class="w-full min-h-[10rem] relative rounded-lg overflow-hidden shadow-md mb-5">
-                            <img class="absolute inset-0 h-full w-full" src="<?= ROOT_URL?>/public/assets/images/fish_images/FISH_IMG_128.jpg" alt="FISH IMAGE">
+                            <img class="absolute inset-0 h-full w-full" src="<?= ROOT_URL?>/public/assets/images/fish_images/<?= $fish->getFish_image() ?>" alt="<?= $fish->getFish_image() ?>">
                         </div>
                         <div class="p-4 bg-slate-200 rounded-lg shadow-[-1px -4px 29px -12px rgba(0,0,0,0.47)]">
                             <p class="text-gray-700 font-semibold">Fish Name: </p>
-                            <p class="text-gray-500 text-sm">Bangus</p>
+                            <p class="text-gray-500 text-sm"><?= $fish->getFish_name() ?></p>
                             <p class="text-gray-700 font-semibold">Scientific Name:</p>
-                            <p class="text-gray-500 text-sm">Malay Ko</p>
+                            <p class="text-gray-500 text-sm"><?= $fish->getScientific_name() ?></p>
                             <p class="text-gray-700 font-semibold">Local Name:</p>
-                            <p class="text-gray-500 text-sm">Malay Ko Ulit</p>
+                            <p class="text-gray-500 text-sm"><?= $fish->getLocal_name() ?></p>
                             <p class="text-gray-700 font-semibold">Family Name: </p>
-                            <p class="text-gray-500 text-sm">Abay ewan ko</p>
+                            <p class="text-gray-500 text-sm"><?= $familyName ?></p>
                         </div>
                     </div>
 
@@ -38,14 +38,16 @@
                     <h1 class="text-center text-xl font-semibold text-gray-700 p-2">Nutritions List</h1>
                     <div class="bg-white p-3 rounded-lg shadow-md mb-10 min-h-[26rem] max-h-[26rem] overflow-auto">
                         <ul class="">
-                            <li class="px-5 py-2 mb-2 text-lg text-gray-700 border-b border-b-gray-700 flex items-center justify-between cursor-pointer hover:bg-slate-200 transition-colors">
-                                <i class="fa-solid fa-hand-point-right text-gray-900"></i>
-                                nutrition item
-                                <div class="btns">
-                                    <a href="/nutritions/edit/2?>" class="py-1 px-4 rounded-sm bg-white text-green-500 hover:bg-green-500 hover:text-white transition-all shadow-md"><i class="fa-solid fa-pencil"></i></a>
-                                    <a href="/nutrition/confirm/2?>" class="py-1 px-4 rounded-sm bg-white text-red-500 hover:bg-red-500 hover:text-white transition-all shadow-md"><i class="fa-solid fa-trash"></i></a>
-                                </div>
-                            </li>
+                            <?php foreach($nutritions as $nutrition) : ?>
+                                <li class="px-5 py-2 mb-2 text-lg text-gray-700 border-b border-b-gray-700 flex items-center justify-between cursor-pointer hover:bg-slate-200 transition-colors">
+                                    <i class="fa-solid fa-hand-point-right text-gray-900"></i>
+                                    <?= $nutrition->getNutrition_name() ?>
+                                    <div class="btns">
+                                        <a href="/nutritions/edit/<?= $fishID?>/<?= $nutrition->getId() ?>" class="py-1 px-4 rounded-sm bg-white text-green-500 hover:bg-green-500 hover:text-white transition-all shadow-md"><i class="fa-solid fa-pencil"></i></a>
+                                        <a href="/nutritions/confirm/<?= $fishID?>/<?= $nutrition->getId() ?>" class="py-1 px-4 rounded-sm bg-white text-red-500 hover:bg-red-500 hover:text-white transition-all shadow-md"><i class="fa-solid fa-trash"></i></a>
+                                    </div>
+                                </li>
+                            <?php endforeach ?>
                             
                         </ul>
                     </div>
@@ -73,8 +75,8 @@
           </div>
         </div>
         <div class="bg-gray-50 px-4 py-4 flex justify-center items center gap-3">
-           <a href="/nutritions/delete/2" type="button" class="min-w-[30%] transition-all rounded-md text-center bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500">Delete</a>
-           <a href="/nutritions/viewNutrition/2" type="button" class="min-w-[30%] transition-all rounded-md text-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Cancel</a>
+          <a href="/nutritions/delete/<?= $fishID ?>/<?= $editNutrition->getId() ?>" type="button" class="min-w-[30%] transition-all rounded-md text-center bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500">Delete</a>
+          <a href="/nutritions/viewNutrition/<?= $fishID ?>" type="button" class="min-w-[30%] transition-all rounded-md text-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Cancel</a>
         </div>
       </div>
     </div>
