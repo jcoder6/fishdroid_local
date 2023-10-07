accaf5acab12367c5437b0d7245fb75edfe6de12
Mark Lester Taguiam <lestertaguiam1107@gmail.com>
2023-10-07 09:00:23 +0800

    fix the ui design in manage-hatchery

--- public/pages/hatchery/manage-hatchery.php
+++ public/pages/hatchery/manage-hatchery.php
@@ -3,9 +3,9 @@
 
 <div class="admin-feature-container w-full h-screen overflow-auto px-3">
 <?php include PAGES_PATH . '/partials/body-header.php' ?>
-   <div class="contents-container w-[80%] mt-[-60px] bg-slate-200 mx-auto rounded-lg"> 
+   <div class="contents-container w-[70%] mt-[-60px] bg-slate-200 mx-auto rounded-lg"> 
       <h4 class="text-2xl text-center font-bold text-gray-700 p-3">Hatchery Process</h4>
-      <div class="manage-header bg-gray-500 flex justify-between items-center px-4 py-2">
+      <div class="manage-header bg-gradient-to-l from-green-400 via-green-500 to-green-600 flex justify-between items-center px-4 py-2">
          <p class="manage-header-title font-bold text-white text-lg">Fish Hathcery List</p>
          <a href="/hatcheries/create" class="bg-white hover:bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-md text-sm font-semibold hover:text-white pr-4 py-1 pl-3 transition-all text-gray-700"><i class="fa-solid fa-plus pr-2"></i>Add Hatch</a>
       </div>
@@ -20,9 +20,9 @@
 
       <?php $num = 0; foreach($hatcheries as $hatchery): $num++ ?>
       <div class="fish-table border-b border-b-gray-400 bg-slate-100 flex px-5 py-2">
-         <div class="text-gray-800 flex items-center font-semibold w-[8%]"><?= $num ?></div>
-         <div class="text-gray-800 flex items-center font-semibold w-[20%]"><?= $hatchery->getFish_name() ?></div>
-         <div class="text-gray-800 flex items-center font-semibold w-[52%] truncate pl-2 pr-8"><?= $hatchery->getHatch_process() ?></div>
+         <div class="text-gray-800 flex items-center w-[8%]"><?= $num ?></div>
+         <div class="text-gray-800 flex items-center w-[20%]"><?= $hatchery->getFish_name() ?></div>
+         <div class="text-gray-800 flex items-center w-[52%] truncate pl-2 pr-8"><?= $hatchery->getHatch_process() ?></div>
          <div class="text-blue-700 flex gap-3 justify-center items-center font-semibold text-center action w-[20%] ml-6">
             <a href="/hatcheries/edit/<?= $hatchery->getId() ?>" class="py-1 px-4 shadow-md bg-white text-green-500 hover:bg-green-500 rounded-md hover:text-white transition-all"><i class="fa-solid fa-pencil"></i></a>
             <a href="/hatcheries/confirm/<?= $hatchery->getId() ?>" class="py-1 px-4 shadow-md bg-white text-red-500 hover:bg-red-500 rounded-md hover:text-white transition-all"><i class="fa-solid fa-trash"></i></a>
