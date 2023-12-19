4ea8a6f7d23e65b6a4cbe449fc58e9dfdafd70c0
Jomer Dorego <jdorego06@gmial.com>
2023-12-19 12:36:31 +0800

    add data to for final defense

--- public/pages/fish/page-fish.php
+++ public/pages/fish/page-fish.php
@@ -3,7 +3,7 @@
 
 <div class="admin-feature-container w-full h-screen overflow-auto px-3">
     <?php include PAGES_PATH . '/partials/body-header.php' ?>
-    <div class="contents-container w-[80%] mt-[-60px] mb-8 bg-slate-100 mx-auto rounded-lg"> 
+    <div class="contents-container w-full mt-[-60px] mb-8 bg-slate-100 mx-auto"> 
         <h4 class="text-xl text-center font-bold text-green-600 p-3">Manage Fish</h4>
         <div class="manage-header bg-gradient-to-l from-green-400 via-green-500 to-green-600 flex justify-between items-center px-4 py-2">
           <p class="manage-header-title font-bold text-white text-lg">Fish List</p>
@@ -22,34 +22,37 @@
         </div>
         <div class="bg-gray-300 flex px-5 py-1">
           <div class="text-gray-700 font-semibold text-center no w-[5%]">No</div>
-          <div class="text-gray-700 font-semibold text-center img w-[25%]">Images</div>
-          <div class="text-gray-700 font-semibold text-left fish-name w-[22%]">Fish Name</div>
-          <div class="text-gray-700 font-semibold text-left img w-[22%]">Scientific Name</div>
-          <div class="text-gray-700 font-semibold text-center action w-[26%]">Action</div>
+          <div class="text-gray-700 font-semibold text-center img w-[20%]">Images</div>
+          <div class="text-gray-700 font-semibold text-left fish-name w-[20%]">Fish Name</div>
+          <div class="text-gray-700 font-semibold text-left img w-[20%]">Scientific Name</div>
+          <div class="text-gray-700 font-semibold text-center action w-[15%]">Action</div>
         </div>
 
         <?php $num = $fishStartCount; foreach($fishes as $fish) : $num++; $fishID = $fish->getId();?>
 
         <div class="fish-table border-b border-b-gray-400 bg-slate-100 flex px-5">
-          <div class="text-gray-800 flex justify-center items-center font-semibold text-center no w-[5%]"><?= $num ?></div>
-          <div class="text-gray-800 flex justify-center items-center text-center img w-[25%] p-1">
-              <div class="fish-image-container w-[30%] h-[50px] overflow-hidden relative">
-                <img src="<?=ROOT_URL?>/public/assets/images/fish_images/<?= $fish->getFish_image() ?>" class="absolute h-[100%] w-[100%] inset-0" alt="Fish Image">
-              </div>
-          </div>
-          <div class="text-gray-800 flex justify-left items-center fish-name w-[22%]"><?= $fish->getFish_name() ?></div>
-          <div class="text-gray-800 flex justify-left items-center img w-[22%]"><?= $fish->getScientific_name() ?></div>
-          <div class="text-blue-700 relative font-semibold text-center action w-[26%]">
-              <div class="fish-menu bg-gradient-to-tr from-green-400 via-green-500 to-green-600 text-white rounded-md shadow-md cursor-pointer px-4 py-1 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"><i class="fa-solid fa-ellipsis"></i></div>
-              <div class="bg-blue-100 shadow-md mt-[7.3rem] hidden flex-col absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10">
-                <a href="/fishes/view/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-blue-500 text-blue-500 hover:text-white"><i class="pr-3 fa-solid fa-eye"></i>View</a></li>
-                <a href="/fishes/edit/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-green-500 text-green-500 hover:text-white transition-all"><i class="pr-3 fa-solid fa-pencil"></i>Edit</a></li>
-                <a href="/recipes/viewRecipe/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-orange-500 text-orange-500 hover:text-white"><i class="pr-3 fa-solid fa-utensils"></i>Recipe</a></li>
-                <a href="/nutritions/viewNutrition/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-purple-500 text-purple-500 hover:text-white"><i class="pr-3 fa-solid fa-heart-pulse"></i>Nutrition</a></li>
-                <a href="/fishes/confirm/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-red-500 text-red-500 hover:text-white"><i class="pr-3 fa-solid fa-trash"></i>Delete</a></li>
-              </div>
-          </div>
-        </div>
+            <div class="text-gray-800 flex justify-center items-center font-semibold text-center no w-[5%]"><?= $num ?></div>
+            <div class="text-gray-800 flex justify-center items-center text-center img w-[20%] p-1">
+               <div class="fish-image-container w-[30%] h-[50px] overflow-hidden relative">
+                  <img src="<?=ROOT_URL?>/public/assets/images/fish_images/<?= $fish->getFish_image() ?>" class="absolute h-[100%] w-[100%] inset-0" alt="Fish Image">
+               </div>
+            </div>
+            <div class="text-gray-800 flex justify-left items-center fish-name w-[20%]"><?= $fish->getFish_name() ?></div>
+            <div class="text-gray-800 flex justify-left items-center img w-[20%]"><?= $fish->getLocal_name() ?></div>
+            <div class="text-gray-800 flex justify-left items-center img w-[20%]"><?= $fish->getScientific_name() ?></div>
+            <div class="text-blue-700 relative font-semibold text-center action w-[15%]">
+               <div class="fish-menu bg-gradient-to-tr from-green-400 via-green-500 to-green-600 text-white rounded-md shadow-md cursor-pointer px-4 py-1 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"><i class="fa-solid fa-ellipsis"></i></div>
+               <div class="bg-blue-100 shadow-md <?= $fish->getIs_edible() ? 'mt-[7.3rem]' : 'mt-[4.9rem]' ?> w-32 hidden flex-col absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10">
+                  <a href="/fishes/view/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-blue-500 text-blue-500 hover:text-white"><i class="pr-3 fa-solid fa-eye"></i>View</a></li>
+                  <a href="/fishes/edit/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-green-500 text-green-500 hover:text-white transition-all"><i class="pr-3 fa-solid fa-pencil"></i>Edit</a></li>
+                  <?php if($fish->getIs_edible()): ?>
+                     <a href="/recipes/viewRecipe/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-orange-500 text-orange-500 hover:text-white"><i class="pr-3 fa-solid fa-utensils"></i>Recipe</a></li>
+                     <a href="/nutritions/viewNutrition/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-purple-500 text-purple-500 hover:text-white"><i class="pr-3 fa-solid fa-heart-pulse"></i>Nutrition</a></li>
+                  <?php endif; ?>
+                  <a href="/fishes/confirm/<?= $fish->getId() ?>" class="text-center p-3 py-2 hover:bg-red-500 text-red-500 hover:text-white"><i class="pr-3 fa-solid fa-trash"></i>Delete</a></li>
+               </div>
+            </div>
+         </div>
         <?php endforeach; ?>   
     </div>
   </div>
